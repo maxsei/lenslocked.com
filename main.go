da5177c832f6d67e0ddbb9162c0978b3199c8ed0
maxsei <maximilliangschulte@gmail.com>
2019-01-18 13:04:00 -0600

    added a contact page

--- main.go
+++ main.go
@@ -6,8 +6,16 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintln(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprintln(w, "<h1>Welcome to my awesome site!</h1>")
+		return
+	}
+    if r.URL.Path == "/contact" {
+        fmt.Fprintf(w, "%s%s\n", "To get in touch, please send an email to ",
+            "<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+    }
+
 }
 
 func main() {
