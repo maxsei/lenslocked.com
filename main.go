a6b19c02bdd3fc3688b1eaf9c0d2aa404c2e74eb
maxsei <maximilliangschulte@gmail.com>
2019-01-19 20:06:49 -0600

    404 added

--- main.go
+++ main.go
@@ -11,11 +11,14 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "<h1>Welcome to my awesome site!</h1>")
 		return
 	}
-    if r.URL.Path == "/contact" {
-        fmt.Fprintf(w, "%s%s\n", "To get in touch, please send an email to ",
-            "<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-    }
-
+	if r.URL.Path == "/contact" {
+		fmt.Fprintf(w, "%s%s\n", "To get in touch, please send an email to ",
+			"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+		return
+	}
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "%s%s", "<h1>We could not find the page you were looking for :(</h1>",
+		"<p>Please email us if you keep being sent to a invalid page.</p>")
 }
 
 func main() {
