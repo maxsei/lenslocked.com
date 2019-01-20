e6b828c3df63429d5c569ecba99f9a3b30b4b8ed
maxsei <maximilliangschulte@gmail.com>
2019-01-19 21:32:23 -0600

    added home and contact handler functions to handle requests routed by gorilla mux

--- main.go
+++ main.go
@@ -3,25 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintln(w, "<h1>Welcome to my awesome site!</h1>")
+
+}
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprintln(w, "<h1>Welcome to my awesome site!</h1>")
-		return
-	}
-	if r.URL.Path == "/contact" {
-		fmt.Fprintf(w, "%s%s\n", "To get in touch, please send an email to ",
-			"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-		return
-	}
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprintf(w, "%s%s", "<h1>We could not find the page you were looking for :(</h1>",
-		"<p>Please email us if you keep being sent to a invalid page.</p>")
+	fmt.Fprintf(w, "%s%s\n", "To get in touch, please send an email to ",
+		"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handlerFunc)
+	r.HandleFunc("/contact", handlerFunc)
+	http.ListenAndServe(":8080", r)
 }
