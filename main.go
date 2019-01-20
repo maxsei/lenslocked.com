ac6dc35870d5b42ec8d503d38b4c8803e72479a8
maxsei <maximilliangschulte@gmail.com>
2019-01-19 21:38:30 -0600

    tried to save creds again

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func contact(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	
+	fmt.Println("saved creds")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
