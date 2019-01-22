4e3277835e42998c95aa4f8416e2bef232633c6e
maxsei <maximilliangschulte@gmail.com>
2019-01-22 11:57:40 -0600

    made first home view

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var homeTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintln(w, "<h1>Welcome to my awesome site!</h1>")
-
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
@@ -18,9 +25,13 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s%s\n", "To get in touch, please send an email to ",
 		"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
-
 func main() {
-	fmt.Println("saved creds")
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
