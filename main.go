7f133e5f40aa922de57d4313aca71a1d34a482f0
maxsei <maximilliangschulte@gmail.com>
2019-01-23 22:46:15 -0600

    created a signup page and a navbar item to get the that page

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,6 +10,7 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
+	signUpView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -25,15 +25,19 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(contactView.Render(w, nil))
 }
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signUpView.Render(w, nil))
+}
 func main() {
-	fmt.Println("Listening and serving on localhost:8080")
-
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	signUpView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":8080", r)
 }
 
