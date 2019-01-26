209ca68ab1e4fa68eb64e0b82c1343d10fb1ca8c
maxsei <maximilliangschulte@gmail.com>
2019-01-26 00:33:25 -0600

    DRYed up the form parsing code into a single function

--- controllers/users.go
+++ controllers/users.go
@@ -29,13 +29,21 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// SignupForm parses information from the sign up page
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 // Create is used to process the signup form when a user
 // submits the sign up form.  This is used to create a new
 // user account .
 // POST signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(w, "Email: %s\tPassword: %s\n", r.PostFormValue("email"), r.PostFormValue("password"))
+	fmt.Fprintln(w, form)
+	// fmt.Fprintf(w, "Email: %s\tPassword: %s\n", form.Email, form.Password)
 }
