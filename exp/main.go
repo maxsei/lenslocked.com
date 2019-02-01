4ef70c2220e0b9d6caa19dada08c9a1761c66726
maxsei <maximilliangschulte@gmail.com>
2019-02-01 13:34:09 -0600

    wrote and tested a token hasher

--- exp/main.go
+++ exp/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 
-	"lenslocked.com/rand"
+	"lenslocked.com/hash"
 )
 
 func main() {
-	fmt.Println(rand.String(10))
-	fmt.Println(rand.RememberToken())
+	toHash := []byte("thisismystringtohash")
+	h := hmac.New(sha256.New, []byte("secretkey"))
+	h.Reset()
+	h.Write(toHash)
+	b := h.Sum(nil)
+	fmt.Println(base64.URLEncoding.EncodeToString(b))
+
+	hmac := hash.NewHMAC("secretkey")
+	fmt.Println(hmac.Hash("thisismystringtohash"))
 }
---
+++ hash/hmac.go
@@ -0,0 +1,30 @@
+package hash
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"hash"
+)
+
+// NewHMAC creates and returns a new HMAC
+func NewHMAC(key string) HMAC {
+	return HMAC{
+		hmac: hmac.New(sha256.New, []byte(key)),
+	}
+}
+
+// HMAC is a wrapper around the crypto/hmax package
+// making it a little easier to use
+type HMAC struct {
+	hmac hash.Hash
+}
+
+// Hash creates a hash of the given input using the private
+// key provided when the HMAC was created
+func (h HMAC) Hash(input string) string {
+	h.hmac.Reset()
+	h.hmac.Write([]byte(input))
+	b := h.hmac.Sum(nil)
+	return base64.URLEncoding.EncodeToString(b)
+}
