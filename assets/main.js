116670798df8f8bc26169f05bac840d8ebaf3afe
maxsei <maximilliangschulte@gmail.com>
2019-02-28 14:17:14 -0600

    stopped running user middleware when serving static assets

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@ for (let i = 0; i < imgs.length; i++){
     let filename = imgs[i].src.split("/").pop()
     if (filename === "dubbzbg.jpg"){
         imgs[i].parentElement.href = "https://www.youtube.com/watch?v=ajlkhFnz8eo"
+        imgs[i].parentElement.target= "_blank"
         imgs[i].addEventListener('click', function(){
                 alert("I'm gay!")
         })
--- middleware/require_user.go
+++ middleware/require_user.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"lenslocked.com/context"
 	"lenslocked.com/models"
@@ -16,6 +18,15 @@ func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 }
 func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		// don't require user middleware if user is requesting
+		// static asset or image so no lookup of user is required
+		if strings.HasPrefix(path, "/assets/") ||
+			strings.HasPrefix(path, "/images/") {
+			next(w, r)
+			return
+		}
+		fmt.Println(path)
 		cookie, err := r.Cookie("remember_token")
 		if err != nil {
 			next(w, r)
